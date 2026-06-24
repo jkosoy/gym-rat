3fc2fa81ec2490052748bd8c46adc1cecda89c28
Jamie Kosoy <jkosoy@users.noreply.github.com>
2026-06-24 15:23:18 +0000

    drag reverse

--- web/src/app/components/WorkoutPicker/touchScroll.utils.ts
+++ web/src/app/components/WorkoutPicker/touchScroll.utils.ts
@@ -3,5 +3,5 @@ export function calculateScrollTop(
   currentY: number,
   startScrollTop: number,
 ): number {
-  return startScrollTop + (currentY - startY);
+  return startScrollTop - (currentY - startY);
 }
